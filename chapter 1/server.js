5e99d47c744ff0b781cefbcd1cc828b7786ebd4a
Shivedra123 <shivendrasaket2580@gmail.com>
2026-05-13 21:55:49 +0530

    student data

--- chapter 1/server.js
+++ chapter 1/server.js
@@ -1,11 +1,55 @@
-const express = require("express");
+const express = require('express')
 
-const app = express();
+const app = express()
+
+const students = [
+      {
+
+        id:1,
+        age: 17,
+        Name: "Shivendra "
+ 
+      },
+   {
+
+      id:2,
+        age: 20,
+        Name: "Aditya Banji baaz" 
+
+   },
+
+
+    {
+      id:3,
+        age: 16,
+        Name: "Akash " 
+
+    }
+
+
+]
 
 app.get("/", (req, res) => {
-    res.send("Hello Shivendra");
+    res.send("Student Backend");
 });
 
-app.listen(3000, () => {
-    console.log("Server Started");
+app.get("/students", (req, res) => {
+    res.send(students);
 });
+
+app.get("/students/:id", (req, res) => {
+
+    const studentId = Number(req.params.id);
+
+    const student = students.find((s) => s.id === studentId);
+
+    res.send(student);
+
+});
+
+
+
+
+app.listen(3000,()=>{
+  console.log(`server is running on  Port 3000`)
+})
